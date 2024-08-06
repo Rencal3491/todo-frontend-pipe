6dd09f438a252b44a0c274bd0e5401ff5182eb39
MJMM <jmj3491329@naver.com>
2024-08-06 17:06:24 +0900

    host fix

--- src/api-config.js
+++ src/api-config.js
@@ -5,7 +5,7 @@ const hostname = window && window.location && window.location.hostname;
 if(hostname === "localhost") {
   backendHost = "http://localhost:8080";
 } else {
-  backendHost = "http://Todo-Backend-Cicd-pipeline-env.eba-24j5bpft.ap-northeast-2.elasticbeanstalk.com";
+  backendHost = "http://todo-backend-cicd-pipeline-env-1.eba-24j5bpft.ap-northeast-2.elasticbeanstalk.com";
 }
 
 export const API_BASE_URL = `${backendHost}`;
