13aa9dc4f4b15b2fdbd0e3476c564a3afc7c7cef
MJMM <jmj3491329@naver.com>
2024-08-06 14:56:17 +0900

    first commit

---
+++ src/api-config.js
@@ -0,0 +1,9 @@
+let backendHost; //백엔드 호스트 주소 결정
+
+const hostname = window && window.location && window.location.hostname;
+
+if(hostname === "localhost") {
+  backendHost = "http://localhost:8080";
+}
+
+export const API_BASE_URL = `${backendHost}`;
